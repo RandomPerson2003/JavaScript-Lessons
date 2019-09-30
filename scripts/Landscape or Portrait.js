8149587509770f3f44bd6f768281a1e367f970fa
RandomPerson03 <54911104+RandomPerson03@users.noreply.github.com>
2019-09-30 13:50:39 -0600

    hkgcseawersdtfyujiomkcgfx

--- scripts/Landscape or Portrait.js
+++ scripts/Landscape or Portrait.js
@@ -2,41 +2,71 @@
 
 let stop = false;
 
-function testNaN (number) {
+function testNaN(number) {
     console.log("stop");
-    if (isNaN (number)) {
+    if (isNaN(number)) {
         stop = true;
         return "Type a number!"
-      } else {
+    } else {
         return "Input Validated"
+    }
+}
+
+function geometry(width, height) {
+    if (width == height) return 'Square';
+    return (width > height) ? 'Landscape' : 'Portrait';
+}
+
+function checkboxes() {
+    let checkbox1, checkbox2, checkbox3;
+    checkbox1 = document.getElementById('Checkbox1').checked;
+    checkbox2 = document.getElementById('Checkbox2').checked;
+    checkbox3 = document.getElementById('Checkbox3').checked;
+    if (checkbox1 == true) {
+        document.getElementById('checkbox1_answer').innerHTML = "Landscape";
+    } else {
+        document.getElementById('checkbox1_answer').innerHTML = "Answer?";
+    }
+    if (checkbox2 == true) {
+        document.getElementById('checkbox2_answer').innerHTML = "Portrait";
+    } else {
+        document.getElementById('checkbox2_answer').innerHTML = "Answer?";
+    }
+    if (checkbox3 == true) {
+        document.getElementById('checkbox3_answer').innerHTML = "Square";
+    } else {
+        document.getElementById('checkbox3_answer').innerHTML = "Answer?";
     }
 }
 
 function main() {
 
-    let firstNumber;
-    let secondNumber;
-    let text;
+    let firstNumber, secondNuber, text
 
     firstNumber = document.getElementById("textfield1").value;
 
-    document.getElementById("validityTest1").innerHTML = test alert (testNaN(firstNumber));
-    document.getElementById("validityTest1").innerHTML = test NaN (firstNuber);
+    document.getElementById("validityTest1").innerHTML = test alert(testNaN(firstNumber));
+    document.getElementById("validityTest1").innerHTML = test NaN(firstNuber);
 
     secondNumber = document.getElementById("textfield2").value;
-    
-    document.getElementById("validityTest2").innerHTML = test alert (testNaN(secondNumber));
-    document.getElementById("validityTest2").innerHTML = test NaN (secondNuber);
+
+    document.getElementById("validityTest2").innerHTML = test alert(testNaN(secondNumber));
+    document.getElementById("validityTest2").innerHTML = test NaN(secondNuber);
 
     if (stop == true) {
-        document.getElementById("answer").innerHTML = "Restart the example, a width or height needs to be a number."
+        document.getElementById("answer").innerHTML = "Restart the example, the width and height need to be a numbers."
     } else {
         console.log("The display geometry is", geometry(firstNumber, secondNumber));
-        document,getElementById('answer').innerHTML = "This display is" + geometry (firstNumber, secondNumber);
+        document, getElementById('answer').innerHTML = "This display is" + geometry(firstNumber, secondNumber);
 
+}
+
+
+    if (stop == true) {
+        document.getElementById('answer').innerHTML = "Restart the example, a width or height needs to be a number."
+    } else {
+        console.log("The display geometry is", geometry(firstNumber, secondNumber));
+        document.getElementById('answer').innerHTML = "This display is " + geometry(firstNumber, secondNumber);
     }
 
-
-    if (stop = false) {}
-    
 }
--- scripts/Max of Two Numbers.js
+++ scripts/Max of Two Numbers.js
@@ -1,1 +1,13 @@
 console.log ("you made it ");
+function max (number1, number2) { 
+  if (number1 > number2) return number1;
+  else if (number1 < number2) return number2;
+  else square = true;
+  console.log("The function didn't run properly");
+}
+
+let square = false;
+
+let number = max (, );
+if (square == true) console.log("The numbers are the same.");
+else console.log("The larger number is", number);
