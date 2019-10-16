06e309152ce1bdca37b29c91595302dce5b06ec1
RandomPerson03 <54911104+RandomPerson03@users.noreply.github.com>
2019-10-16 13:49:58 -0600

    update!

--- scripts/Demerit Points.js
+++ scripts/Demerit Points.js
@@ -1,6 +1,5 @@
 console.log("You made it to the console!");
 
-
 let stop = false;
 
 function testNaN(number) {
@@ -15,52 +14,23 @@ function testNaN(number) {
 
 function demeritCalculation(speedLimit, speed) {
 
-    var i;    
 
-    for (i = 0; i < 15; i++) {
-        console.log ("The number is " + i + "<br>");
-      }
-
-    if (speed < speedLimit) return 'Speed is less than the speed limit';
-    if (speed < speedLimit + 1 * 5) return 'Speed limit has been exceeded, but by a minimal amount. No demerit points have been recieved';
-    if (speed < speedLimit + 2 * 5) return '1 demerit point';
-    if (speed < speedLimit + 3 * 5) return '2 demerit point';
-    if (speed < speedLimit + 4 * 5) return '3 demerit point';
-    if (speed < speedLimit + 5 * 5) return '4 demerit point';
-    if (speed < speedLimit + 6 * 5) return '5 demerit point';
-    if (speed < speedLimit + 7 * 5) return '6 demerit point';
-    if (speed < speedLimit + 8 * 5) return '7 demerit point';
-    if (speed < speedLimit + 9 * 5) return '8 demerit point';
-    if (speed < speedLimit + 10 * 5) return '9 demerit point';
-    if (speed < speedLimit + 11 * 5) return '10 demerit point';
-    if (speed < speedLimit + 12 * 5) return '11 demerit point';
-    if (speed < speedLimit + 13 * 5) return '12 demerit point';
-    if (speed < speedLimit + 14 * 5) return '13 demerit point';
-    if (speed >= speedLimit + 14 * 5) return 'You have exceeded the 13 demerit point limit, and your license has been suspended';
-    return 'Check algorithm for errors';
-}
-
-
-function checkboxes() {
-    let checkbox1, checkbox2, checkbox3;
-    checkbox1 = document.getElementById('Checkbox1').checked;
-    checkbox2 = document.getElementById('Checkbox2').checked;
-    checkbox3 = document.getElementById('Checkbox3').checked;
-    if (checkbox1 == true) {
-        document.getElementById('checkbox1_answer').innerHTML = "Speed?";
-    } else {
-        document.getElementById('checkbox1_answer').innerHTML = "Answer?";
-    }
-    if (checkbox2 == true) {
-        document.getElementById('checkbox2_answer').innerHTML = "Speed?";
-    } else {
-        document.getElementById('checkbox2_answer').innerHTML = "Answer?";
-    }
-    if (checkbox3 == true) {
-        document.getElementById('checkbox3_answer').innerHTML = "Speed?";
-    } else {
-        document.getElementById('checkbox3_answer').innerHTML = "Answer?";
-    }
+    if (speedLimit > speed) return 'Speed is less than the speed limit';
+    if (1*5 + speedLimit > speed) return 'Speed limit has been exceeded, but by a minimal amount. No demerit points have been recieved.';
+    if (2*5 + speedLimit > speed) return '1 demerit point recieved.';
+    if (3*5 + speedLimit > speed) return '2 demerit points recieved.';
+    if (4*5 + speedLimit > speed) return '3 demerit points recieved.';
+    if (5*5 + speedLimit > speed) return '4 demerit points recieved.';
+    if (6*5 + speedLimit > speed) return '5 demerit points recieved.';
+    if (7*5 + speedLimit > speed) return '6 demerit points recieved.';
+    if (8*5 + speedLimit > speed) return '7 demerit points recieved.';
+    if (9*5 + speedLimit > speed) return '8 demerit points recieved.';
+    if (10*5 + speedLimit > speed) return '9 demerit points recieved.';
+    if (11*5 + speedLimit > speed) return '10 demerit points recieved.';
+    if (12*5 + speedLimit > speed) return '11 demerit points recieved.';
+    if (13*5 + speedLimit > speed) return '12 demerit points recieved.';
+    if (14*5 + speedLimit >= speed) return 'You have exceeded the 13 demerit point limit, and your license has been suspended';
+    return 'Check for errors!';
 }
 
 
@@ -69,24 +39,20 @@ function main() {
 
     let firstNumber, secondNumber;
 
+  
+
     firstNumber = document.getElementById('textfield1').value;
 
     if (firstNumber == '') {
         firstNumber = 70;
-    } else if (firstNumber != '') {} else {
-        console.log("Speed Limit errors in JavaScript file.");
     }
 
     document.getElementById('validityTest1').innerHTML = alert(testNaN(firstNumber));
     document.getElementById('validityTest1').innerHTML = testNaN(firstNumber);
 
     secondNumber = document.getElementById('textfield2').value;
-    console.log(secondNumber);
     if (secondNumber == '') {
         stop = true;
-        secondNumber = 0;
-    } else if (secondNumber != '') {} else {
-        console.log("error calculating demerit points in JavaScript file.");
     }
 
     document.getElementById('validityTest2').innerHTML = alert(testNaN(secondNumber));
@@ -94,14 +60,11 @@ function main() {
 
     document.getElementById('speedLimit').innerHTML = firstNumber;
     document.getElementById('speed').innerHTML = secondNumber;
-
+    
     if (stop == true) {
         document.getElementById('answer').innerHTML = "Please retry, both inputs need to numbers that are above zero."
     } else {
         console.log("Demerit Calculation is: ", demeritCalculation(firstNumber, secondNumber));
-        document.getElementById('answer').innerHTML = "Demerit Calculation is: " + demeritCalculation(firstNumber, secondNumber);
+        document.getElementById('answer').innerHTML = "" + demeritCalculation(firstNumber, secondNumber);
     }
 }
-
-
-for ()
--- scripts/Greeting Someone.js
+++ scripts/Greeting Someone.js
@@ -1,1 +1,54 @@
 console.log("You made it to the console!");
+
+let morning = false;
+let afternoon = false;
+let response;
+
+function testNaN(number) {
+    if (isNaN(number)) {
+        stop = true;
+        return "Type a real number"
+    } else {
+        return "Input Validated"
+    }
+}
+
+
+function stuff(afternoon, morning,) {
+
+    if (morning == true) {
+        response = ('Good Morning!');
+        console.log("Good Morning!");
+    }
+    if (afternoon == true) {
+        response = ("Good Afternoon!");
+        console.log("Good Afternoon!")
+    } 
+}
+
+function main() {
+
+    let firstNumber, morning, afternoon;
+
+    firstNumber = document.getElementById('textfield1').value;
+
+    document.getElementById('validityTest1').innerHTML = alert(testNaN(firstNumber));
+    document.getElementById('validityTest1').innerHTML = testNaN(firstNumber);
+
+    if (firstNumber <= 1159) {
+        morning = true
+    }
+    if (firstNumber >= 1200) {
+        afternoon = true
+    }
+
+
+
+    if (stop == true){
+        console.log("Please retry, the input needs to be a number above zero.")
+    } else {
+        stuff(afternoon, morning)
+        document.getElementById('answer').innerHTML = response;
+    }
+
+}
